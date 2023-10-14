c9fa8990ca7c9df249592dd819bff0855eec2134
Ayokunnumi1 <omololuayk@gmail.com>
2023-10-14 08:03:41 +0100

    Fix error issues and include search input in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,31 @@
+import { useDispatch } from 'react-redux';
+import { filterMeals } from '../redux/Meals/MealSlice';
 import mealLogo from '../asset/meal logo.png';
 import '../modules/Header.css';
 
-const Header = () => (
-  <header>
-    <div className="header-left">
-      <img src={mealLogo} alt="meal logo" style={{ width: '40px' }} />
-      <h1>Ayk foods</h1>
-    </div>
-    <p className="nutrient-header">Nutrient Diary</p>
-  </header>
-);
+const Header = () => {
+  const dispatch = useDispatch();
+  const searchMeals = (e) => {
+    const searchValue = e.target.elements.searchInput.value;
+    e.preventDefault();
+    dispatch(filterMeals(searchValue));
+  };
+  return (
+    <header>
+      <div className="header-left">
+        <img src={mealLogo} alt="meal logo" style={{ width: '40px' }} />
+        <h1>Ayk foods</h1>
+      </div>
+      <form onSubmit={searchMeals} id="form-input">
+        <input
+          type="text"
+          placeholder="find chicken soup"
+          id="user-input"
+          name="searchInput"
+        />
+      </form>
+    </header>
+  );
+};
 
 export default Header;
